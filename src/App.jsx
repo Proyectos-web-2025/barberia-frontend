b94212ca0904d9b14172ea665a98955fe6414d44
Proyectos-web-2025 <proyectos2025.web@gmail.com>
2025-07-18 21:33:08 -0500

    feat: aplicativo configurado para utilizar React Router

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,8 @@ import "./App.css";
 function App() {
   return (
     <>
-      <h1 class="text-3xl font-bold underline">Hello world!</h1>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <h2>Prueba de rutas</h2>
     </>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,40 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+// Importacion de la libreria para utilizar react-router
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router";
+
+// importaciones para renderizar la aplicacion
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 import './index.css'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+// Define las rutas de tu aplicación con sus respectivos componentes.
+const router = createBrowserRouter(
+  [
+    {
+      // Ruta raíz ("/") que renderiza el componente App.
+      path: "/",
+      element: <App />,
+    },
+  ],
+  {
+    /**
+     * "basename" especifica la base del path cuando se despliega en una subcarpeta,
+     * en GitHub Pages dentro de /barberia-frontend.
+     */
+    basename: "/barberia-frontend",
+  }
+);
+
+// Obtiene el elemento HTML donde se montará la aplicación React.
+const root = document.getElementById("root");
+
+
+// Renderiza el router usando RouterProvider.
+// Esto permite que toda la app tenga acceso a la navegación por rutas.
+ReactDOM.createRoot(root).render(
+  <RouterProvider router={router} />
+);
