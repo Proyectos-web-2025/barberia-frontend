83e9b8cd4f9c16487402b739ea3fcdbb4e904c94
Proyectos-web-2025 <proyectos2025.web@gmail.com>
2025-07-20 12:22:18 -0500

    feat: se agregó diseño base con Tailwind y configuración inicial

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import "./App.css";
+import "./index.css";
 
 function App() {
   return (
     <>
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
-      <h2>Prueba de rutas</h2>
+      <h2 className="text-amber-400"> Prueba de rutas</h2>
     </>
   );
 }
